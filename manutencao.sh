ee44f232ace5fcc8e1490757d41532c326cd1d0a
maryk30 <kurien.shreya@gmail.com>
2026-03-07 02:06:34 +0000

    skeleton files

---
+++ manutencao.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# SO_HIDE_DEBUG=1                   ## Uncomment this line to hide all @DEBUG statements
+# SO_HIDE_COLOURS=1                 ## Uncomment this line to disable all escape colouring
+. so_utils.sh                       ## This is required to activate the macros so_success, so_error, and so_debug
+
+#####################################################################################
+## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2025/2026, Enunciado Versão 1
+##
+## Aluno: Nº:       Nome:
+## Nome do Módulo: manutencao.sh
+## Descrição/Explicação do Módulo:
+##
+##
+#####################################################################################
+
+##/**
+## * @brief  s3_1_Manutencao Ler a descrição da tarefa S3.1 no enunciado
+## */
+s3_1_Manutencao () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+main () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+main "$@" # This statement invokes the function main(), with all the arguments passed by the user
+
+## S3.2. Invocação do script:
+## • Altere o ficheiro cron.def fornecido, por forma a configurar o seu sistema para que o Script: manutencao.sh  seja executado todos os dias de segunda-feira a sábado (incluindo feriados), quando tiver passado um minuto da meia-noite (às 0h01). Nos comentários no início do ficheiro cron.def, explique a configuração realizada, e indique qual é o comando Shell associado a essa configuração que vai ser utilizado para despoletar essa configuração.
+## • O ficheiro cron.def alterado deverá ser submetido para avaliação juntamente com os outros Shell scripts
+## • Não deverá ser acrescentado nenhum código neste ficheiro manutencao.sh para responder a esta alínea, todas as respostas deverão ser realizadas no ficheiro cron.def.
---
+++ menu.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# SO_HIDE_DEBUG=1                   ## Uncomment this line to hide all @DEBUG statements
+# SO_HIDE_COLOURS=1                 ## Uncomment this line to disable all escape colouring
+. so_utils.sh                       ## This is required to activate the macros so_success, so_error, and so_debug
+
+#####################################################################################
+## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2025/2026, Enunciado Versão 1
+##
+## Aluno: Nº:       Nome:
+## Nome do Módulo: menu.sh
+## Descrição/Explicação do Módulo:
+##
+##
+#####################################################################################
+
+## Este script invoca os scripts restantes, não recebendo argumentos.
+## Atenção: Não é suposto que volte a fazer nenhuma das funcionalidades dos scripts anteriores. O propósito aqui é simplesmente termos uma forma centralizada de invocar os restantes scripts.
+
+##/**
+## * @brief  s5_1_MostraMenu Ler a descrição da tarefa S5.1 no enunciado.
+## *         Esta função preenche o valor da variável $opcao com a opção dada pelo utilizador
+## */
+s5_1_MostraMenu () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_2_1_ValidaOpcao Ler a descrição da tarefa S5.2.1 no enunciado
+## */
+s5_2_1_ValidaOpcao () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_2_2_ProcessaOpcao Ler a descrição da tarefa S5.2.2 no enunciado
+## */
+s5_2_2_ProcessaOpcao () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_3_Opcao1 Ler a descrição da tarefa S5.3 no enunciado
+## */
+s5_3_Opcao1 () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_4_Opcao2 Ler a descrição da tarefa S5.3 no enunciado
+## */
+s5_4_Opcao2 () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_5_Opcao3 Ler a descrição da tarefa S5.3 no enunciado
+## */
+s5_5_Opcao3 () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+##/**
+## * @brief  s5_6_Opcao4 Ler a descrição da tarefa S5.3 no enunciado
+## */
+s5_6_Opcao4 () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+
+main () {
+    so_debug "<"
+
+    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+
+    so_debug ">"
+}
+main "$@" # This statement invokes the function main(), with all the arguments passed by the user
