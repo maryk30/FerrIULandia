9df807e7c5aca63c7b03ba73b3f0d877a81ef7b0
maryk30 <kurien.shreya@gmail.com>
2026-03-09 12:51:20 +0000

    Update daily_limit in materiais.txt based on previous day's sales.

--- manutencao.sh
+++ manutencao.sh
@@ -6,7 +6,7 @@
 #####################################################################################
 ## ISCTE-IUL: Trabalho prático de Sistemas Operativos 2025/2026, Enunciado Versão 1
 ##
-## Aluno: Nº:       Nome:
+## Aluno: Nº: 141820      Nome: Shreya Mary Kurien
 ## Nome do Módulo: manutencao.sh
 ## Descrição/Explicação do Módulo:
 ##
@@ -17,17 +17,75 @@
 ## * @brief  s3_1_Manutencao Ler a descrição da tarefa S3.1 no enunciado
 ## */
 s3_1_Manutencao () {
+
+    # Check if materiais.txt exists
     so_debug "<"
 
-    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+    if [ -f materiais.txt ]; then
+        if [ -r materiais.txt ] && [ -w materiais.txt ]; then
+            so_success 3.1.1 "materiais.txt exists with read/write permissions."
+        else
+            so_error 3.1.1 "materiais.txt exists without read/write permissions."
+        fi
+    else
+        so_error 3.1.1 "materiais.txt does not exist"
+    fi
 
     so_debug ">"
+
+    so_debug "<"
+
+    if [ -f vendas.txt ]; then
+        if [ -r vendas.txt ] && [ -w vendas.txt ]; then
+            so_success 3.1.1 "vendas.txt exists with read/write permissions."
+        else
+            so_error 3.1.1 "vendas.txt exists without read/write permissions."
+        fi
+    fi
+
+    so_debug ">"
+
+    # Update daily limit
+    daily_limit=$(awk -v mat="$material" '$1==mat {print $3; exit}' materiais.txt)
+
+    yesterday=$(date -d "yesterday" +%F)
+
+    weight_sold=$(awk -F ";" -v mat="$material" -v d="$yesterday" '
+    $2==mat && substr($4,1,10)==d {sum+=$3}
+    END {print sum+0}
+    ' vendas.txt)
+
+    while IFS=";" read -r material price daily_limit
+    do
+        # calculate total sold today for this material
+        weight_sold=$(awk -F ";" -v mat="$material" -v d="$yesterday" '
+        $2==mat && substr($4,1,10)==d {sum+=$3}
+        END {print sum+0}
+        ' vendas.txt)
+
+        so_debug "< {material:$material;daily_limit:$daily_limit;weight_sold:$weight_sold}"
+
+        if (( weight_sold == daily_limit )); then
+            new_limit=$((daily_limit + 100))
+
+            # update the limit in materiais.txt
+            awk -F ";" -v mat="$material" -v new="$new_limit" '
+            BEGIN{OFS=";"}
+            $1==mat {$3=new}
+            {print}
+            ' materiais.txt > temp && mv temp materiais.txt
+
+            so_success 2.X "Daily limit for $material increased to $new_limit"
+        fi
+
+    done < materiais.txt
+
 }
 
 main () {
     so_debug "<"
 
-    ##// Substituir este comentário pelo código a ser implementado pelo aluno
+    s3_1_Manutencao
 
     so_debug ">"
 }
